4c0aea80930b1894323d8cb93458221bed6c5e26
Gal Pressman <galpressman@gmail.com>
2017-01-08 16:28:22 +0200

    tests: Encrypt test token
    Our testing now assumes we have the needed environment variable instead of
    publicly keeping it in our repository.
    
    Signed-off-by: Gal Pressman <galpressman@gmail.com>

--- tests/init_config.sh
+++ tests/init_config.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+# This test assumes you have $TG_TOKEN evironment variable defined
 
 set -e
 set -x
 
 CONFIG_FILE="config.json"
-TOKEN="314662038:AAGyo7vcZpS89xoe59gd075MEioBRHEc5wQ"
 
 cp -f config.json.example $CONFIG_FILE
-sed -i -- "s/my_telegram_token_here/""${TOKEN}""/g" $CONFIG_FILE
+set +x
+sed -i -- "s/my_telegram_token_here/""${TG_TOKEN}""/g" $CONFIG_FILE
